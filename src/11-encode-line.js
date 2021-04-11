d07bbf67261fc9ca03455abdb0cb411c70a37b48
VityaRa <blackops2in@mail.ru>
2021-04-11 21:39:08 +0300

    feat: add code for 11th task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let counter = 0;
+  let letter = str[0];
+
+  if (!str.length) return result;
+
+  str.split('').forEach((x) => {
+    if (x === letter) {
+      counter += 1;
+      return;
+    }
+
+    const temp = counter !== 1 ? `${counter}${letter}` : letter;
+    result = `${result}${temp}`;
+    letter = x;
+    counter = 1;
+  });
+
+  return counter !== 1 ? `${result}${counter}${letter}` : `${result}${letter}`;
 }
 
 module.exports = encodeLine;
